b120d9fc5b78fde560cad17833b4c3799aa00902
Ahmad55-debug <classifying8@gmail.com>
2026-08-23 23:18:21 -0700

    Add Day 1 exercise for Python learning

---
+++ python_learning/day1_exercise.py
@@ -0,0 +1,28 @@
+# Day 1: Python Basics
+# Exercise 1: Self Introduction
+
+name = "Ahmad Hassan"
+city = "Faisalabad"
+age = 20
+
+print("Name:", name)
+print("City:", city)
+print("Age:", age)
+
+# Crop Information
+crop_name = "Wheat"
+crop_height = 1.3
+
+print("Crop Name:", crop_name, "Crop Height:", crop_height, "cm")
+
+# Adittion
+num1 = 10
+num2 = 20
+sum = num1 + num2
+print("Sum:", sum)
+
+# Area Calculation
+Length = 12
+width = 8
+area = Length * width
+print("field area:", area, "square units")
