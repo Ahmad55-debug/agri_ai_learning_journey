861a7bcfd22f3b42787211cf67007029490930a2
Ahmad55-debug <classifying8@gmail.com>
2026-08-23 23:38:40 -0700

    Add age calculator and number analysis script

---
+++ python_learning/day2_exercise.py
@@ -0,0 +1,39 @@
+# Age calculator
+birth_year = 2005
+
+current_year = 2026
+
+age = current_year - birth_year
+print("Age:", age)
+
+# Calculate number is odd or even
+number = 7
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+    # Calculate the biggest number
+    num1 = 10
+    num2 = 20
+    if num1 > num2:
+        print("The biggest number is :", num1)
+    else:
+        print("The biggest number is :", num2)
+
+        # calculates grades of students
+        marks = 95
+        marks = 85
+        marks = 75
+        marks = 65
+        marks = 55
+        if marks >= 90:
+            print("Grade:", "A")
+        elif marks >= 80:
+            print("Grade:", "B")
+        elif marks >= 70:
+            print("Grade:", "C")
+        elif marks >= 60:
+            print("Grade:", "D")
+        else:
+            print("Grade:", "F")
