a04bbde04d604f3b79435ba7148d9cc040494b81
Ahmad55-debug <classifying8@gmail.com>
2026-08-26 22:27:12 -0700

    Add grade calculation program
    This program calculates the grade based on predefined scores.

---
+++ python_learning/day3_exercise.py
@@ -0,0 +1,17 @@
+# This program calculates the grade based on the score.
+a = 90
+b = 80
+c = 70
+d = 60
+e = 50
+
+if a >= 90:
+    print("Grade will be:", "A")
+elif b >= 80:
+    print("Grade will be:", "B")
+elif c >= 70:
+    print("Grade will be:", "C")
+elif d >= 60:
+    print("Grade will be:", "D")
+else:
+    print("Grade will be:", "F")
