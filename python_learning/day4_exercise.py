8bb41c3c10e9fad685737c81643ba731262e0c1e
Ahmad55-debug <classifying8@gmail.com>
2026-08-27 20:47:27 -0700

    Add day4_exercise.py with various calculations

---
+++ python_learning/day4_exercise.py
@@ -0,0 +1,33 @@
+
+price = float(input("PRICE:"))
+quantity = float(input("QUANTITY"))
+
+total = price * quantity
+
+print("TOTAL BILL IS:", total)
+
+first = float(input("First subject marks:"))
+second = float(input("second subject marks:"))
+third = float(input("third subject marks:"))
+
+total = first + second + third
+
+print("AVG:", (total/300) * 100)
+
+a = 10
+b = 20
+
+sum = a + b / 2
+print(sum)
+
+lenth = float(input("enter first value:"))
+width = float(input("enter second value:"))
+
+perimeter = 2 * (lenth + width)
+
+print("perimeter:", perimeter)
+
+age = int(input("Enter your age:"))
+future_age = age + 10
+
+print("After 10 years age will be:", future_age)
