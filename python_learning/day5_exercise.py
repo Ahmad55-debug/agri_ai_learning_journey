17d53e10726e6405713ffc9b88e64ac6296f9b3c
Ahmad55-debug <classifying8@gmail.com>
2026-09-01 22:32:00 -0700

    Add string manipulation examples in day5_exercise.py
    This script demonstrates string length measurement, indexing, and slicing.

---
+++ python_learning/day5_exercise.py
@@ -0,0 +1,16 @@
+# This is measure lenth of string
+city = "faisalabad"
+
+print(len(city))
+
+# Indexing of string
+
+print(city[0]) # first character.
+
+print(city[-1]) # last character
+
+print(city[-3:-1]) # negative indexing
+
+print(city[0:5]) # slicing from index 0 to 4
+
+print(city[2:5]) # slicing from index 2 to 4
