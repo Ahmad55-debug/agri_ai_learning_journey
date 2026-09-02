c23e9b182170c8c6db4f431e69847f910bebad41
Ahmad55-debug <classifying8@gmail.com>
2026-09-01 22:37:28 -0700

    Add string manipulation functions in day6_exercise.py

---
+++ python_learning/day6_exercise.py
@@ -0,0 +1,12 @@
+# String Function
+
+birth = "i am born in pakistan"
+print(birth.endswith("an")) # check if string ends with "an"
+
+print(birth.startswith("i")) # check if string starts with "i"
+
+print(birth.find("born")) # find the index of the substring "born"
+
+print(birth.count("a")) # count the occurrences of the character "a"
+
+print(birth.replace("i am ", "i was "))
